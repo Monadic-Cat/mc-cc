59371737f1cfabd913e9e40a41ffde093c3c1761
Monadic Cat <greenmonadcat@gmail.com>
2021-10-26 01:48:49 -0500

    comment and test something

--- startup.lua
+++ startup.lua
@@ -5,8 +5,12 @@ system_dir = "/" .. fs.getDir(system_startup)
 
 path_additions = ":".. system_dir .. "/bin"
 
+-- If we're not booting from the root directory, we're booting from a disk.
+-- Disk booting gives access to programs that only make sense to run when doing so.
 if not (system_dir == "/") then
    path_additions = path_additions .. ":" .. system_dir .. "/bin/live"
+   settings.set("system_dir", system_dir)
+   os.hmmm = 11
 end
 
 shell.setPath(shell_path .. path_additions)
