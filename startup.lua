e20090243fb2a35f091ebeb9c906b4481122c0c0
Monadic Cat <greenmonadcat@gmail.com>
2021-10-26 01:14:23 -0500

    fix thing

--- startup.lua
+++ startup.lua
@@ -1,12 +1,12 @@
 local shell_path = shell.path()
 
 system_startup = shell.getRunningProgram()
-system_dir = fs.getDir(system_startup)
+system_dir = "/" .. fs.getDir(system_startup)
 
-path_additions = ":".. "/" .. system_dir .. "/bin"
+path_additions = ":".. system_dir .. "/bin"
 
 if not (system_dir == "/") then
-   path_additions = path_additions .. ":" .. "/" .. system_dir .. "/bin/live"
+   path_additions = path_additions .. ":" .. system_dir .. "/bin/live"
 end
 
 shell.setPath(shell_path .. path_additions)
