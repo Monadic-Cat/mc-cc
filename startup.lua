f6f068ce594490160f22bc8b42d26cc565eea1aa
Monadic Cat <greenmonadcat@gmail.com>
2021-10-26 01:12:06 -0500

    add to path if booting from disk

--- startup.lua
+++ startup.lua
@@ -3,4 +3,10 @@ local shell_path = shell.path()
 system_startup = shell.getRunningProgram()
 system_dir = fs.getDir(system_startup)
 
-shell.setPath(shell_path .. ":".. "/" .. system_dir .. "bin")
+path_additions = ":".. "/" .. system_dir .. "/bin"
+
+if not (system_dir == "/") then
+   path_additions = path_additions .. ":" .. "/" .. system_dir .. "/bin/live"
+end
+
+shell.setPath(shell_path .. path_additions)
